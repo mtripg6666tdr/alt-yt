96c65f2dbb463a18775eb8c190e601140fef8908
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-02-27 17:08:28 +0900

    Fix: send headers twice

--- src/server/util/error.ts
+++ src/server/util/error.ts
@@ -5,6 +5,10 @@ import { Response } from "express";
 export const errorTemplate = fs.readFileSync(path.join(__dirname, "../../common/error.html"), {encoding:"utf-8"});
 
 export function respondError(res:Response, message:string, status:number = 500){
-  res.writeHead(status, {"Content-Type": "text/html; charset=UTF-8"});
-  res.end(errorTemplate.replace(/{message}/, status + "<br>" + message));
+  if(res.headersSent){
+    res.end();
+  }else{
+    res.writeHead(status, {"Content-Type": "text/html; charset=UTF-8"});
+    res.end(errorTemplate.replace(/{message}/, status + "<br>" + message));
+  }
 }
